c5363761774b247f3893d5f217c65ca91d3af892
Abhiram Madhu <abhirammadhunote@gmail.com>
2023-10-25 11:56:13 +0530

    first commit

---
+++ lib/common_widget/bottom_nav_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../view/home_screen/home_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+int currentIndex = 0;
+final screens = [
+  const HomeScreen(),
+  
+];
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) => setState(
+          () => currentIndex = index,
+        ),
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        // backgroundColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+            ),
+            label: '',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.local_convenience_store),
+          //   label: '',
+          // ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.note_add_outlined),
+          //   label: '',
+          // ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
